ee0925666fdae6f8c351cac62badb5e90a4c0fcc
alekseiNagornyi <alexnaf1999@yandex.ru>
2021-07-14 19:26:28 +0300

    Feature 1 / Создание шаблонов 3 страницы

---
+++ proj/resources/views/contact.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('title-block')
+
+Контакты
+
+@endsection
+
+
+@section('content')
+
+<h1>Страница контактов</h1>
+
+@endsection
---
+++ proj/resources/views/inc/hero.blade.php
@@ -0,0 +1,7 @@
+<div class="jumbotron">
+  <div class="container">
+    <h1>Hello everyone</h1>
+    <p>lit anim id est laborum.
+    </p>
+  </div>
+</div>
